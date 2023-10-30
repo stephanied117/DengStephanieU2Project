5ea920cbf4d7a8eff45f25e0c2c056628503daf8
stephanied117 <stephanied117@nycstudents.net>
2023-10-30 19:13:37 -0400

    changes to methods

--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -13,14 +13,19 @@ public class LinearEquationLogic {
     }
 
     public void start() {
+        /*
         System.out.println("(.//c//.) <(Welcome!)");
         point1();
         point2();
         getCoordinates1();
         getCoordinates2();
         LinearEquation more = new LinearEquation(x1, y1, x2, y2);
+        System.out.println(lineInfo());
+        System.out.println("(*////*) <(Enter an x value!)");
+
     }
-    private boolean coordinateFormat1() {
+
+    public boolean coordinateFormat1() {
         if (!(p1.substring(0,1).equals("("))) {
             return correct = false;
         }
@@ -33,7 +38,7 @@ public class LinearEquationLogic {
         return correct = true;
     }
 
-    private boolean coordinateFormat2() {
+    public boolean coordinateFormat2() {
         if (!(p2.substring(0,1).equals("("))) {
             return correct = false;
         }
@@ -46,7 +51,7 @@ public class LinearEquationLogic {
         return correct = true;
     }
 
-    private void point1() {
+    public void point1() {
         Scanner scan = new Scanner(System.in);
         System.out.println("(u//c//u) <(Please enter a coordinate point!) ");
         String pointA = scan.nextLine();
@@ -58,7 +63,7 @@ public class LinearEquationLogic {
             p1 = pointA;
         }
     }
-    private void point2() {
+    public void point2() {
         Scanner scan = new Scanner(System.in);
         System.out.println("(u//c//u) <(Please enter another coordinate point!) ");
         String pointB = scan.nextLine();
@@ -89,44 +94,16 @@ public class LinearEquationLogic {
         y2 = Integer.parseInt(p2.substring(letter + 2, p2.length() - 1));
     }
 
-/*
-    public void start() {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Welcome! \nPlease enter a coordinate point: ");
-        LinearEquation point = new LinearEquation();
-        String pointA = scan.nextLine();
-        System.out.println("Please enter another coordinate point: ");
-        String pointB = scan.nextLine();
-        getCoordinates(pointA, pointB);
+    public static void main(String[] args) {
+        System.out.println("(.//c//.) <(Welcome!)");
 
+        LinearEquationLogic yay = new LinearEquationLogic(x1, y1, x2, y2);
+        yay.point1();
+        yay.point2();
+        yay.getCoordinates1();
+        yay.getCoordinates2();
+        LinearEquation more = new LinearEquation(x1, y1, x2, y2);
+        System.out.println(more.lineInfo());
+        System.out.println("(*//o//*) <(Enter an x value!)");
     }
-
-    public void getCoordinates(String pointA, String pointB) {
-        p1 = pointA;
-        p2 = pointB;
-    }
-    public void point1() {
-        int letter = 0;
-        while (!p1.substring(letter, letter + 1).contains(",")) {
-            letter ++;
-        }
-        x1 = Integer.parseInt(p1.substring(1,letter - 1));
-        y1 = Integer.parseInt(p1.substring(letter + 2, p1.length() - 1));
-    }
-    public void point2() {
-        int letters = 0;
-        while (!p2.substring(letters, letters + 1).contains(",")) {
-            letters ++;
-        }
-        x2 = Integer.parseInt(p2.substring(1,letters - 1));
-        y2 = Integer.parseInt(p2.substring(letters + 2, p2.length() - 1));
-    }
-    public double calcSlope() {
-        if ((y2 - y1) % (x2 - x1) == 0) {
-            return (y2 - y1) / (x2 - x1);
-        }
-        return (double)((y2 - y1) + "/" + (x2 - x1));
-    }
- */
-
 }
