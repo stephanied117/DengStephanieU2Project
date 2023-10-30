619979a994b339398d3ca5b3fafb81c036835ae5
stephanied117 <stephanied117@nycstudents.net>
2023-10-30 16:50:33 -0400

    more methods

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,64 @@
+import java.lang.Math;
 public class LinearEquation {
+    private int x1;
+    private int x2;
+    private int y1;
+    private int y2;
+    private int p1;
+    private int p2;
+    public LinearEquation(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    public double distance() {
+        return roundToHundredth(Math.pow(Math.pow(Math.abs(x2 - x1), 2) + Math.pow(Math.abs(y2 - y1), 2), 0.5));
+    }
+
+    public double yIntercept() {
+        return roundToHundredth(((((y2 - y1) / (x2 - x1)) * x1) - y1));
+    }
+
+    public double slope() {
+        if (y2 - y1 == 0) {
+            return 0;
+        }
+        if (x2 - x1 == 0) {
+            return 0.123456789;
+        }
+        if ((y2 - y1) % (x2 - x1) == 0) {
+            return (y2 - y1) / (x2 - x1);
+        }
+        return Integer.parseInt((y2 - y1) + "/" + (x2 - x1));
+    }
+
+    public String equation() {
+        if (slope() == 0) {
+            return "y = " + yIntercept();
+        } else if (slope() == 0.123456789) {
+            return "x = " + x1;
+        }
+        if (yIntercept() > 0) {
+            return "y = " + slope() + "x + " + yIntercept();
+        } else if (yIntercept() == 0) {
+            return "y = " + slope() + "x";
+        } else if (yIntercept() < 0) {
+            return "y = " + slope() + "x - " + Math.abs(yIntercept());
+        }
+        return "Line does not exist";
+    }
+
+    public String coordinateForX(double x) {
+        return "(" + x + ", " + roundToHundredth(slope() * x + yIntercept()) + ")";
+    }
+
+    public String lineInfo() {
+        return "The two points are: " + p1 + " and " + p2 + "\nThe equation of the line between these points is : " + equation() + "\nThe slope of this line is: " + (y2 - y1) / (x2 - x1) + "\nThe y-intercept of this line is: " + yIntercept() + "\nThe distance between these points is " + distance();
+    }
+
+    private double roundToHundredth(double toRound) {
+        return ((int)((toRound + 0.005) * 100.0) / 100.0);
+    }
 }
--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.lang.Math;
 public class LinearEquationLogic {
     private String p1;
     private String p2;
@@ -6,7 +7,89 @@ public class LinearEquationLogic {
     private int y1;
     private int x2;
     private int y2;
+    private boolean correct;
 
+    public LinearEquationLogic() {
+    }
+
+    public void start() {
+        System.out.println("(.//c//.) <(Welcome!)");
+        point1();
+        point2();
+        getCoordinates1();
+        getCoordinates2();
+        LinearEquation more = new LinearEquation(x1, y1, x2, y2);
+    }
+    private boolean coordinateFormat1() {
+        if (!(p1.substring(0,1).equals("("))) {
+            return correct = false;
+        }
+        if (!(p1.substring(p1.length() - 1, p1.length()).equals(")"))) {
+            return correct = false;
+        }
+        if (!(p1.contains(", "))) {
+            return correct = false;
+        }
+        return correct = true;
+    }
+
+    private boolean coordinateFormat2() {
+        if (!(p2.substring(0,1).equals("("))) {
+            return correct = false;
+        }
+        if (!(p2.substring(p2.length() - 1, p2.length()).equals(")"))) {
+            return correct = false;
+        }
+        if (!(p2.contains(", "))) {
+            return correct = false;
+        }
+        return correct = true;
+    }
+
+    private void point1() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("(u//c//u) <(Please enter a coordinate point!) ");
+        String pointA = scan.nextLine();
+        p1 = pointA;
+        coordinateFormat1();
+        while (correct = false) {
+            System.out.println("(Q//C//Q) <(You are using an INCORRECT format. Please follow the (int1, int2) format! Ex: (1, 2).)");
+            pointA = scan.nextLine();
+            p1 = pointA;
+        }
+    }
+    private void point2() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("(u//c//u) <(Please enter another coordinate point!) ");
+        String pointB = scan.nextLine();
+        p2 = pointB;
+        coordinateFormat2();
+        while (correct = false) {
+            System.out.println("(Q//C//Q) <(You are using an INCORRECT format. Please follow the (int1, int2) format! Ex: (1, 2).)");
+            pointB = scan.nextLine();
+            p2 = pointB;
+        }
+    }
+
+    public void getCoordinates1() {
+        int letter = 0;
+        while (!p1.substring(letter, letter + 1).contains(",")) {
+            letter ++;
+        }
+        x1 = Integer.parseInt(p1.substring(1, letter));
+        y1 = Integer.parseInt(p1.substring(letter + 2, p1.length() - 1));
+    }
+
+    public void getCoordinates2() {
+        int letter = 0;
+        while (!p2.substring(letter, letter + 1).contains(",")) {
+            letter ++;
+        }
+        x2 = Integer.parseInt(p2.substring(1,letter - 1));
+        y2 = Integer.parseInt(p2.substring(letter + 2, p2.length() - 1));
+    }
+
+/*
     public void start() {
         Scanner scan = new Scanner(System.in);
         System.out.println("Welcome! \nPlease enter a coordinate point: ");
@@ -42,10 +125,8 @@ public class LinearEquationLogic {
         if ((y2 - y1) % (x2 - x1) == 0) {
             return (y2 - y1) / (x2 - x1);
         }
-        return 0;
-        /*
-        return (y2 - y1) + "/" + (x2 - x1);
-         */
+        return (double)((y2 - y1) + "/" + (x2 - x1));
     }
+ */
 
 }
